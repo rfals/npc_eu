1491c1c268dd81294189609a67b63c2a64c38f3a
Reinis <reinisfals95@gmail.com>
2024-04-07 18:37:31 +0300

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Estimating Phillips Curve and the output gap in the Eurozone by using explainable neural networks.',
+    author='Reinis Fals Igors Tatarinovs',
+    license='',
+)
